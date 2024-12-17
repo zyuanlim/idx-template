d26048dee63c47f03675413427edbb2220659af5
Zane Lim <zyuanlim@gmail.com>
2024-12-17 21:17:25 +0800

    Fix wrong package.

--- langgraph/.idx/dev.nix
+++ langgraph/.idx/dev.nix
@@ -11,7 +11,7 @@
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
     pkgs.python312
-    pkgs.python312Packages.pip
+    pkgs.python312Packages.pip-tools
   ];
   # Sets environment variables in the workspace
   env = {};
